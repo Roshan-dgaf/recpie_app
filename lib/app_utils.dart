4050a1bb286dcb1a852936a13eaf39892f6085ee
Roshan Baidar <roshanbaidar1234@gmail.com>
2024-12-13 16:35:12 +0545

    add colour green

--- lib/app_utils.dart
+++ lib/app_utils.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const Color colorWhite = Colors.white;
 const Color colorPrimary = Color(0xFF5145C1);
 const Color colorGrey = Colors.grey;
+const Color colorGreen = Colors.green;
