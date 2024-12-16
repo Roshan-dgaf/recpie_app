67c2c70635c272f6cad77baa3e9fa52a819670db
Roshan Baidar <roshanbaidar1234@gmail.com>
2024-12-16 08:13:39 +0545

    a

--- lib/app_utils.dart
+++ lib/app_utils.dart
@@ -5,3 +5,4 @@ const Color colorPrimary = Color(0xFF5145C1);
 const Color colorGrey = Colors.grey;
 const Color colorGreen = Colors.green;
 const Color colorblack = Colors.black;
+const Color colorred = Colors.red;
