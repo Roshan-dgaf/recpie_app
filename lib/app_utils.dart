59269dd18de2ec19c8ef6bb570b0c6ea3cb1f717
Roshan Baidar <roshanbaidar1234@gmail.com>
2025-01-31 20:26:30 +0545

    fix

--- lib/app_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color colorWhite = Colors.white;
-const Color colorPrimary = Color(0xFF5145C1);
-const Color colorGrey = Colors.grey;
-const Color colorGreen = Colors.green;
-const Color colorblack = Colors.black;
-const Color colorred = Colors.red;
-// const Color colorSecondary = Color(0x8278DA);
