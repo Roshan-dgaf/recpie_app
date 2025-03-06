557c530bee146d0293ef49f26839d8f09b18dba7
Roshan Baidar <roshanbaidar1234@gmail.com>
2025-03-06 21:25:51 +0545

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:softwarica_student_management_bloc/app/app.dart';
-import 'package:softwarica_student_management_bloc/app/di/di.dart';
-import 'package:softwarica_student_management_bloc/core/network/hive_service.dart';
+import 'package:hive/hive.dart';
+import 'package:recipe_app/app/app.dart';
+import 'package:recipe_app/app/di/di.dart';
+import 'package:recipe_app/core/network/hive_service.dart';
+import 'package:recipe_app/feature/Product/data/model/product_hive_model.dart';
+import 'package:recipe_app/feature/auth/data/model/auth_hive_model.dart';
+import 'package:recipe_app/feature/order/data/model/order_hive_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await HiveService.init();
-
-  // await HiveService().clearStudentBox();
-
+  Hive.registerAdapter(AuthHiveModelAdapter());
+  Hive.registerAdapter(ProductHiveModelAdapter());
+  Hive.registerAdapter(OrderHiveModelAdapter());
+  await HiveService().init();
   await initDependencies();
 
-  runApp(App());
+  runApp(
+    const MyApp(),
+  );
 }
