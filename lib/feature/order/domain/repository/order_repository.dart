436256fa747cd81d148ee107aa8f393fa19d7f1f
Roshan Baidar <roshanbaidar1234@gmail.com>
2025-03-06 21:01:52 +0545

    features_and_dashbaord

---
+++ lib/feature/order/domain/repository/order_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:recipe_app/core/error/failure.dart';
+import 'package:recipe_app/feature/Order/domain/entity/order_entity.dart';
+
+abstract interface class IOrderRepository {
+  Future<Either<Failure, List<OrderEntity>>> getOrder(
+      String? token, String userId);
+  Future<Either<Failure, void>> createOrder(OrderEntity order);
+  Future<Either<Failure, void>> deleteOrder(String id);
+}
